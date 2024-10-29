e33401654b5201f168f771bbceece049e47f98dd
plpe2 <147789736+plpe2@users.noreply.github.com>
2024-10-29 12:30:37 +0800

    Completed Assignment 1: Fibonacci Sequence (need adjustment)

--- nestjs-assignments/src/assignments/assignments.controller.ts
+++ nestjs-assignments/src/assignments/assignments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
 export class AssignmentsController {
@@ -7,6 +7,11 @@ export class AssignmentsController {
     fibonacci(@Param('n') n : number ) : { sequence : number[]}{
         //Initialize yung first two number ng fibonacci sequence
         let sequence = [0, 1];
+
+        //Checking if yung number is less than 1 or walang nilagay na input 
+        if (isNaN(n) || n < 1) {
+            throw new BadRequestException('Parameter must be a positive integer');
+        }
 
         //for loop: i is eqauls to 2 kase nakainitialize na yung 0 at 1, at yung 2 yung start ng counting
         for (let i = 2; i < n; i++) {
